19b69caf3a51b34bd6f61df540f5c409913eb19e
DiamondMuffin24 <146829200+DiamondMuffin24@users.noreply.github.com>
2024-10-15 16:16:39 +0100

    tunnl done

--- Assets/HeyYou.cs
+++ Assets/HeyYou.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HeyYou : MonoBehaviour, IInteractable
 {
+    public GameObject hut;
+    public GameObject textbox;
+
     public void Interact()
-{
-    
-}
+    {
+        Debug.Log("interact");
+        textbox.SetActive(true);
+        StartCoroutine(TeleportAfterDelay(1f)); // Start the coroutine
+    }
+
+    private IEnumerator TeleportAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay); // Wait for 15 seconds
+
+        Debug.Log("delay");
+
+        // Load the desired scene (replace "YourSceneName" with the actual scene name)
+        SceneManager.LoadScene("innner_dock");
+    }
+
 }
